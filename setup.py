c5517bfc935bcaa5524b5df010fe8b8a49eb3ec7
Jens Vagelpohl <jens@netz.ooo>
2019-04-15 22:39:42 -0500

    - vb [ci skip]

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def _read(fname):
 
 setup(
     name='Products.Sessions',
-    version='4.5',
+    version='4.6.dev0',
     url='https://github.com/zopefoundation/Products.Sessions/',
     project_urls={
         'Issue Tracker': ('https://github.com/zopefoundation'
