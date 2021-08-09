936216fec33cab4f83b99908469f5a0ffb95e8a2
dieter <dieter@handshake.de>
2021-08-09 07:06:33 +0200

    make `isort` happy

--- src/Products/Sessions/tests/testBrowserIdManager.py
+++ src/Products/Sessions/tests/testBrowserIdManager.py
@@ -653,10 +653,10 @@ class TestBrowserIdManager(unittest.TestCase):
     def test_new_browser_id(self):
         from time import time
 
-        from .. BrowserIdManager import getB64TStampToInt
-        from .. BrowserIdManager import getBrowserIdPieces
-        from .. BrowserIdManager import getNewBrowserId
-        from .. BrowserIdManager import isAWellFormedBrowserId
+        from ..BrowserIdManager import getB64TStampToInt
+        from ..BrowserIdManager import getBrowserIdPieces
+        from ..BrowserIdManager import getNewBrowserId
+        from ..BrowserIdManager import isAWellFormedBrowserId
         start = time()
         bid = getNewBrowserId()
         self.assertEqual(len(bid), 19)
