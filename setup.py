b7134e4bde98444e37d0b251537208526b95d3f8
Jens Vagelpohl <jens@plyp.com>
2025-04-05 09:12:47 +0200

    - vb [ci skip]

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def _read(fname):
 
 setup(
     name='Products.Sessions',
-    version='6.0',
+    version='6.1.dev0',
     url='https://github.com/zopefoundation/Products.Sessions/',
     project_urls={
         'Issue Tracker': ('https://github.com/zopefoundation'
